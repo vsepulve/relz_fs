7daa9734e672ea338f73b19933160e1ecc17bb50
vsepulve <vsepulve@dcc.uchile.cl>
2017-08-18 12:55:17 -0300

    Minor corrections to messages.

--- src/compress.cpp
+++ src/compress.cpp
@@ -40,100 +40,94 @@ int main(int argc, char* argv[]){
 		return 0;
 	}
 	
-	const char *entrada_referencia = argv[1];
-	const char *entrada_comprimir = argv[2];
-	const char *nombre_salida = argv[3];
+	const char *reference_text = argv[1];
+	const char *sequence_text = argv[2];
+	const char *output = argv[3];
 	unsigned int block_size = atoi(argv[4]);
 	unsigned int n_threads = atoi(argv[5]);
-	const char *referencia_serializada = argv[6];
-	bool construir_referencia = (atoi(argv[7]) == 1);
+	const char *serialized_reference = argv[6];
+	bool build_reference = (atoi(argv[7]) == 1);
 	bool use_metadata = (atoi(argv[8]) == 1);
 	//Dejo la base de golomb por defecto (2^6)
 	
-	cout<<"Inicio (referencia \""<<entrada_referencia<<"\", procesar \""<<entrada_comprimir<<"\", salida \""<<nombre_salida<<"\")\n";
+	cout<<"Start (reference \""<<reference_text<<"\", sequence \""<<sequence_text<<"\", output \""<<output<<"\")\n";
 	
 	BitsUtils utils;
 	NanoTimer timer;
 	
-	ReferenceIndex *referencia = NULL;
+	ReferenceIndex *reference = NULL;
 //	TextFilter *filter = new TextFilterBasic();
 	TextFilter *filter = new TextFilterFull();
 	
-	if(construir_referencia){
+	if(build_reference){
 	
 		char *text = NULL;
 		unsigned int text_size = 0;
 		
-		fstream lector(entrada_referencia, fstream::in);
-		if( ! lector.good() ){
-			cerr<<"Error leyendo Referencia\n";
+		fstream reader(reference_text, fstream::in);
+		if( ! reader.good() ){
+			cerr<<"Error reading Reference\n";
 			return 0;
 		}
-		lector.seekg (0, lector.end);
-		unsigned int largo_text = lector.tellg();
-		lector.seekg (0, lector.beg);
-		lector.close();
+		reader.seekg (0, reader.end);
+		unsigned int text_len = reader.tellg();
+		reader.seekg (0, reader.beg);
+		reader.close();
 		
 //		//Agregacion de N's y del alfabeto valido completo
-		unsigned int largo_ns = 1024;
-		vector<char> alfabeto;
-		filter->getAlphabet( &alfabeto );
+		unsigned int ns_len = 1024;
+		vector<char> alphabet;
+		filter->getAlphabet( &alphabet );
 		
-		text = new char[largo_text + largo_ns + alfabeto.size() + 1];
-//		text = new char[largo_text + largo_ns + 1];
+		text = new char[text_len + ns_len + alphabet.size() + 1];
+//		text = new char[text_len + ns_len + 1];
 		
-		
-		// TEMPORALMENTE COMENTADO MIENTRAS PRUEBO LA BUSQUEDA
 		//Agregacion de N's y del alfabeto valido completo
-		for(unsigned int i = 0; i < largo_ns; ++i){
+		for(unsigned int i = 0; i < ns_len; ++i){
 			text[text_size++] = 'N';
 		}
-		for(unsigned int i = 0; i < alfabeto.size(); ++i){
-			text[text_size++] = alfabeto[i];
+		for(unsigned int i = 0; i < alphabet.size(); ++i){
+			text[text_size++] = alphabet[i];
 		}
 		
-		
 		//Carga del texto de la referencia (metodo especial para cargar bases solamente)
-		text_size += filter->readReference(entrada_referencia, text + text_size);
+		text_size += filter->readReference(reference_text, text + text_size);
 		
-		cout<<"Construyendo Referencia con texto de "<<text_size<<" chars\n";
+		cout<<"Building Reference with "<<text_size<<" chars\n";
 		timer.reset();
-		//ReferenceIndex *referencia = new ReferenceIndexBasic(text);
-		referencia = new ReferenceIndexBasic(text, n_threads);
-		cout<<"Construido en "<<timer.getMilisec()<<" ms\n";
+		reference = new ReferenceIndexBasic(text, n_threads);
+		cout<<"Built in "<<timer.getMilisec()<<" ms\n";
 		
 		timer.reset();
-		referencia->save(referencia_serializada);
-		cout<<"Guardado en "<<timer.getMilisec()<<" ms\n";
-		//----- FIN CONSTRUCCION REFERENCIA -----
+		reference->save(serialized_reference);
+		cout<<"Saved in "<<timer.getMilisec()<<" ms\n";
 		
 		delete [] text;
 	}
 	else{
-		cout<<"Cargando Referencia\n";
+		cout<<"Loading Reference\n";
 		timer.reset();
 		
-		referencia = new ReferenceIndexBasic();
-		referencia->load(referencia_serializada);
-		cout<<"Referencia Cargada en "<<timer.getMilisec()<<" ms\n";
+		reference = new ReferenceIndexBasic();
+		reference->load(serialized_reference);
+		cout<<"Reference loaded in "<<timer.getMilisec()<<" ms\n";
 	}
 	
 	//Usando el Compressor
 	CompressorSingleBuffer compressor(
-		nombre_salida, 
-		new CoderBlocksRelz(referencia), 
-		new DecoderBlocksRelz(referencia->getText()), 
+		output, 
+		new CoderBlocksRelz(reference), 
+		new DecoderBlocksRelz(reference->getText()), 
 		filter
 		);
 		
 	timer.reset();
-	compressor.compress(entrada_comprimir, n_threads, block_size, use_metadata);
-	cout<<"Compresion terminada en "<<timer.getMilisec()<<" ms\n";
+	compressor.compress(sequence_text, n_threads, block_size, use_metadata);
+	cout<<"Compression finished in "<<timer.getMilisec()<<" ms\n";
 	
-	cout<<"Borrando\n";
-	delete referencia;
+	delete reference;
 	
-	cout<<"Fin\n";
+	cout<<"End\n";
 	
 }
 
--- src/decompress.cpp
+++ src/decompress.cpp
@@ -30,34 +30,33 @@ int main(int argc, char* argv[]){
 	if(argc != 5){
 		cout<<"\nUsage: decompress serialized_reference compressed_file output_text buffer_size\n";
 		cout<<"The program reads the files \"compressed_file\" and the text from \"serialized_reference\".\n";
-		cout<<"It the writes the text in \"output_text\" using a decompression block of length \"buffer_size\".\n";
+		cout<<"It writes the text in \"output_text\" using a decompression block of length \"buffer_size\".\n";
 		cout<<"\n";
 		return 0;
 	}
 	
-	const char *referencia_serializada = argv[1];
-	const char *entrada_comprimida = argv[2];
-	const char *nombre_salida = argv[3];
+	const char *serialized_reference = argv[1];
+	const char *compressed_file = argv[2];
+	const char *output_text = argv[3];
 	unsigned int buffer_size = atoi(argv[4]);
 	
-	cout<<"Inicio (referencia \""<<referencia_serializada<<"\", comprimida \""<<entrada_comprimida<<"\", salida \""<<nombre_salida<<"\", block_size "<<buffer_size<<")\n";
+	cout<<"Start (reference \""<<serialized_reference<<"\", compressed \""<<compressed_file<<"\", output \""<<output_text<<"\", block_size "<<buffer_size<<")\n";
 	
-	cout<<"Cargando texto referencia\n";
+	cout<<"Loading reference text\n";
 	NanoTimer timer;
 	
-	char *reference_text = ReferenceIndexBasic::loadText(referencia_serializada);
+	char *reference_text = ReferenceIndexBasic::loadText(serialized_reference);
 	
 	//Usando el Compressor
-	CompressorSingleBuffer compressor(entrada_comprimida, NULL, new DecoderBlocksRelz(reference_text));
+	CompressorSingleBuffer compressor(compressed_file, NULL, new DecoderBlocksRelz(reference_text));
 	
 	timer.reset();
-	compressor.decompress(nombre_salida, buffer_size);
-	cout<<"Descompresion terminada en "<<timer.getMilisec()<<" ms\n";
+	compressor.decompress(output_text, buffer_size);
+	cout<<"Decompression finished in "<<timer.getMilisec()<<" ms\n";
 	
-	cout<<"Borrando decoder...\n";
 	delete [] reference_text;
 	
-	cout<<"Fin\n";
+	cout<<"End\n";
 	
 }
 
