603662df8db90519bc25a7e70ee9f794611361a5
vsepulve <vsepulve@dcc.uchile.cl>
2018-04-13 14:06:37 -0300

    ReferenceFactory added (to load Basic or RR versions depending on the file).

---
+++ include/ReferenceFactory.h
@@ -0,0 +1,37 @@
+#ifndef _REFERENCE_FACTORY_H
+#define _REFERENCE_FACTORY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+#include "NanoTimer.h"
+
+#include "ReferenceIndex.h"
+#include "ReferenceIndexBasic.h"
+#include "ReferenceIndexRR.h"
+
+class ReferenceFactory{
+
+private: 
+
+public: 
+	ReferenceFactory();
+	virtual ~ReferenceFactory();
+	
+	// Reads the type so it can load and return the right instance of Reference
+	static ReferenceIndex *loadInstance(const char *ref_file);
+	
+	// Reads and returns only the text from a reference file
+	// It will reserve the necesary memory
+	static char *loadText(const char *ref_file);
+	
+};
+
+#endif //_REFERENCE_FACTORY_H
+
---
+++ src/ReferenceFactory.cpp
@@ -0,0 +1,63 @@
+#include "ReferenceFactory.h"
+
+ReferenceFactory::ReferenceFactory(){}
+
+ReferenceFactory::~ReferenceFactory(){}
+
+ReferenceIndex *ReferenceFactory::loadInstance(const char *ref_file){
+	
+	fstream reader(ref_file, fstream::binary | fstream::in);
+	if( ! reader.good() ){
+		cerr << "ReferenceIndexRR::load - Error opening file \"" << ref_file << "\"\n";
+		return NULL;
+	}
+	
+	unsigned char type = 0;
+	reader.read((char*)&type, 1);
+	reader.close();
+	
+	ReferenceIndex *ref = NULL;
+	
+	if( type == 1 ){
+		cout << "ReferenceFactory - Loading ReferenceIndexBasic\n";
+		ref = new ReferenceIndexBasic();
+		ref->load(ref_file);
+	}
+	else if( type == 2 ){
+		cout << "ReferenceFactory - Loading ReferenceIndexRR\n";
+		ref = new ReferenceIndexRR();
+		ref->load(ref_file);
+	}
+	else{
+		cerr << "ReferenceFactory - Unexpected Reference Type (" << (unsigned int)type << ")\n";
+	}
+	
+	return ref;
+}
+
+// Notar que este metodo es static
+char *ReferenceFactory::loadText(const char *ref_file){
+	char *text = NULL;
+	unsigned char type = 0;
+	unsigned int text_size = 0;
+	unsigned int arr_size = 0;
+	
+	fstream reader(ref_file, fstream::binary | fstream::in);
+	if( ! reader.good() ){
+		cerr<<"ReferenceFactory::loadText - Error al abrir archivo.\n";
+		return NULL;
+	}
+	
+	reader.read((char*)&type, 1);
+	reader.read((char*)(&text_size), sizeof(int));
+	reader.read((char*)(&arr_size), sizeof(int));
+	cout<<"ReferenceFactory::loadText - Loading reference text (" << text_size << " chars) from \"" << ref_file << "\" (type: " << (unsigned int)type << ")\n";
+	
+	text = new char[text_size + 1];
+	reader.read(text, text_size);
+	text[text_size] = 0;
+	
+	reader.close();
+	
+	return text;
+}
