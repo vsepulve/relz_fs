efb44a52a481a392ac5ea9acbfe314a290da4b38
vsepulve <vsepulve@dcc.uchile.cl>
2018-10-01 11:32:38 -0300

    Baseline to add the processing and storage of the compressed and isolated fasta metadata.

---
+++ include/MetadataFasta.h
@@ -0,0 +1,43 @@
+#ifndef _METADATA_FASTA_H
+#define _METADATA_FASTA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include <vector>
+#include <map>
+
+using namespace std;
+class MetadataFasta {
+
+public:
+	
+protected: 
+	
+public: 
+	
+	MetadataFasta();
+	
+	virtual ~MetadataFasta();
+	
+	unsigned long long filterMetadata(char *text, unsigned long long text_length);
+	
+};
+
+
+
+
+
+
+
+#endif //_METADATA_FASTA_H
+
+
+
+
+
---
+++ src/MetadataFasta.cpp
@@ -0,0 +1,19 @@
+#include "MetadataFasta.h"
+
+MetadataFasta::MetadataFasta(){
+	
+}
+
+MetadataFasta::~MetadataFasta(){
+	
+}
+
+	
+unsigned long long MetadataFasta::filterMetadata(char *text, unsigned long long text_length){
+	return text_length;
+}
+
+
+
+
+
